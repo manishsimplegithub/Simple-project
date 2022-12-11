dd3b5bd203d094c68a2d563ebf4b21a2b8684ad6
manishsimplegithub <manishsrivastav@bbdu.ac.in>
2022-12-11 18:38:36 +0530

    Updates

---
+++ precache-manifest.e5a9cde3a6c20e9ea3a4138e378b6c53.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d3430ba846dc6f2b241",
+    "url": "/Simple-project/static/css/main.e3f9a113.chunk.css"
+  },
+  {
+    "revision": "9d3430ba846dc6f2b241",
+    "url": "/Simple-project/static/js/main.bcbe910c.chunk.js"
+  },
+  {
+    "revision": "ff8a32dbe01836ddb524",
+    "url": "/Simple-project/static/js/runtime~main.aa2a7db9.js"
+  },
+  {
+    "revision": "f3841669466a8df69555",
+    "url": "/Simple-project/static/css/2.d9e23854.chunk.css"
+  },
+  {
+    "revision": "f3841669466a8df69555",
+    "url": "/Simple-project/static/js/2.39814ab8.chunk.js"
+  },
+  {
+    "revision": "2c6b046a7682591c78d0",
+    "url": "/Simple-project/static/js/3.0dd53fc1.chunk.js"
+  },
+  {
+    "revision": "a4e5e1d120a225c88d3acbb2b427988c",
+    "url": "/Simple-project/static/media/web.a4e5e1d1.jpg"
+  },
+  {
+    "revision": "14a0ce4298a15a8b665653b409746067",
+    "url": "/Simple-project/static/media/OIP.14a0ce42.jpeg"
+  },
+  {
+    "revision": "334859d2207b2bf0e15f34d8c206af0f",
+    "url": "/Simple-project/static/media/second.334859d2.webp"
+  },
+  {
+    "revision": "c3946173adc86e5f28193b6ce01b809e",
+    "url": "/Simple-project/static/media/app.c3946173.jpg"
+  },
+  {
+    "revision": "ae00bb043255ad651a9d52356dcf3fcc",
+    "url": "/Simple-project/static/media/software.ae00bb04.jpg"
+  },
+  {
+    "revision": "3942283e9e0cf8031f2a3ac821c57f06",
+    "url": "/Simple-project/static/media/digital.3942283e.jpg"
+  },
+  {
+    "revision": "a516313fc18d5225b5334058feb243c9",
+    "url": "/Simple-project/static/media/flutter.a516313f.jpg"
+  },
+  {
+    "revision": "3466b0e9f41acbf72f40b56196821e27",
+    "url": "/Simple-project/static/media/java.3466b0e9.jpg"
+  },
+  {
+    "revision": "96c3dd10f143dd886ffdcc697774023b",
+    "url": "/Simple-project/index.html"
+  }
+];
