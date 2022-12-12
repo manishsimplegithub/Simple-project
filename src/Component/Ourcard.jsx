f39ebf12d510343a2804b844a680dad23c86980c
manishsimplegithub <manishsrivastav@bbdu.ac.in>
2022-12-12 14:09:13 +0530

    changes

--- src/Component/Ourcard.jsx
+++ src/Component/Ourcard.jsx
@@ -14,7 +14,7 @@ const Ourcard = (props) => {
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
-            <a href="" className="btn btn-primary">
+            <a href="/" className="btn btn-primary">
               Go somewhere
             </a>
           </div>
